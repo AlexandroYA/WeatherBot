77276bdc2f33327e2cb80c9c7c6e9528cdd7c0dc
AlexandroYA <84091990+AlexandroYA@users.noreply.github.com>
2021-05-31 12:55:22 +0500

    Update config.py

--- config.py
+++ config.py
@@ -2,3 +2,4 @@
 TOKEN = '1756808541:AAHWF5u4KxgQohRnkI1QYopys0XVm8ntzAg'
 # ТОКЕН БОТА
 sticker = 'CAACAgIAAxkBAANAYGgwMyhFwYWh2-3O-glFfxP6Oa0AAl4JAAJ5XOIJ1ceysiubHYweBA'
+sticker2 = 'CAACAgQAAxkBAAIbo2CukS6YjiohsrYRkYa6picrvWvhAAJmAAP44AQCLDkxgUcZcAgfBA'
