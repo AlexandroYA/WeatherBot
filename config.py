8aba1cb7f39d6fac2253fcc645230357baef91b9
AlexandroYA <84091990+AlexandroYA@users.noreply.github.com>
2021-06-10 14:30:00 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 #Используется для хранения всех постоянных.
-TOKEN = '1756808541:AAHWF5u4KxgQohRnkI1QYopys0XVm8ntzAg'
+TOKEN = СЕКРЕТНО
 # ТОКЕН БОТА
 sticker = 'CAACAgIAAxkBAANAYGgwMyhFwYWh2-3O-glFfxP6Oa0AAl4JAAJ5XOIJ1ceysiubHYweBA'
 sticker2 = 'CAACAgIAAxkBAAIF_2BtqtWdZ7LvTNXZuWHRWpZHImQoAAL1AwACcBFhCNq3TDN6JU9hHgQ'
